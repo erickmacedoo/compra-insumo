60e4af5c48268d591fdf5af862604027d6c821ce
Erick Macedo <erickmacedo@edu.unifor.br>
2026-05-25 22:30:03 -0300

    3️⃣ Criando as classes RegraNegocioException e GlobalExceptionHandler

---
+++ src/main/java/feira/packages/exception/GlobalExceptionHandler.java
@@ -0,0 +1,20 @@
+package feira.packages.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    
+    @ExceptionHandler(RegraNegocioException.class)
+    public ResponseEntity<String> handleRegraNegocioException(RegraNegocioException ex) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
+    }
+    
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<String> handleIllegalArgumentException(IllegalArgumentException ex) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
+    }
+}
